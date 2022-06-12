60045e0b598438a5a7b2a958d23b8d425619d03d
Diana Mendybaeva <DRMendybaeva@gmail.com>
2022-06-13 00:13:59 +0500

    fix import

---
+++ src/games/brain-calc.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+import {greetAndGetUserName, generateRandomNum, isExpectedAnswerEqualUserAnswer } from '../index.js';
+
+// games
+const gameBrainCalc = () => {
+  const userName = greetAndGetUserName();
+  console.log('What is the result of the expression?');
+
+  let successRound = 0;
+
+  for (let i = 0; i < 3; i += 1) {
+    const randomNum1 = generateRandomNum(0, 100);
+    const randomNum2 = generateRandomNum(0, 100);
+    const mathOperator = ['+', '-', '*'];
+    const randomIndex = generateRandomNum(0, 2);
+    const randomMathOperator = mathOperator[randomIndex];
+    const calcQuestion = `${randomNum1} ${randomMathOperator} ${randomNum2}`;
+
+    console.log(`Question: ${calcQuestion}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    const getExpectedAnswer = () => {
+      switch (randomMathOperator) {
+        case '+':
+          return randomNum1 + randomNum2;
+        case '-':
+          return randomNum1 - randomNum2;
+        case '*':
+          return randomNum1 * randomNum2;
+        default:
+          return null;
+      }
+    };
+    const expectedAnswer = String(getExpectedAnswer());
+    const ExpectedAnswerEqualUserAnswer = isExpectedAnswerEqualUserAnswer(
+      expectedAnswer,
+      userAnswer,
+      userName,
+    );
+    if (ExpectedAnswerEqualUserAnswer) {
+      successRound += 1;
+    } else {
+      break;
+    }
+  }
+  if (successRound === 3) {
+    console.log(`Congratulation, ${userName}!`);
+  }
+};
+export default gameBrainCalc;
---
+++ src/games/brain-even.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+import {greetAndGetUserName, generateRandomNum, isExpectedAnswerEqualUserAnswer } from '../index.js';
+
+// games
+const gameBrainEven = () => {
+  const userName = greetAndGetUserName();
+  console.log('Answer "yes" if the number is even, otherwise answer "no"');
+
+  let successRound = 0;
+
+  for (let i = 0; i < 3; i += 1) {
+    const rundomNum = generateRandomNum(0, 100);
+    console.log(`Question: ${rundomNum}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    const isEven = rundomNum % 2 === 0;
+    const expectedAnswer = isEven === true ? 'yes' : 'no';
+
+    const ExpectedAnswerEqualUserAnswer = isExpectedAnswerEqualUserAnswer(
+      expectedAnswer,
+      userAnswer,
+      userName,
+    );
+    if (ExpectedAnswerEqualUserAnswer) {
+      successRound += 1;
+    } else {
+      break;
+    }
+  }
+  if (successRound === 3) {
+    console.log(`Congratulation, ${userName}!`);
+  }
+};
+export default gameBrainEven;
