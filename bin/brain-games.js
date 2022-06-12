44881979087b809e570ceb3ff016b5863e56ccc6
Diana Mendybaeva <DRMendybaeva@gmail.com>
2022-06-13 00:11:26 +0500

    change brain-even (add general games algorithm)

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-import setUserName from '../src/cli.js';
+import {greetAndGetUserName} from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-
-const userName = setUserName();
-console.log(`Hello, ${userName}!`);
+greetAndGetUserName();
