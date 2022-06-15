3f65272ce932d65b78babdd32d29148e44087a51
Diana Mendybaeva <DRMendybaeva@gmail.com>
2022-06-15 18:04:35 +0500

    fix cli.js(only console.log functions

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,45 @@
 import readlineSync from 'readline-sync';
 
-const setUserName = () => {
+export const setUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
   return userName;
 };
 
-export default setUserName;
+export const greetAndGetUserName = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = setUserName();
+  console.log(`Hello, ${userName}!`);
+  return userName;
+};
+export const displayGameRules = (gameName) => {
+  switch (gameName) {
+    case 'gameBrainEven':
+      return console.log('Answer "yes" if the number is even, otherwise answer "no"');
+    case 'gameBrainCalc':
+      return console.log('What is the result of the expression?');
+    default:
+      return null;
+  }
+};
+export const askQuestion = (question) => {
+  console.log(`Question: ${question}`);
+};
+export const isExpectedAnswerEqualUserAnswer = (
+  expectedAnswer,
+  userAnswer,
+) => expectedAnswer === userAnswer;
+export const displayMessageAfterCorrectRound = () => {
+  console.log('Correct!');
+};
+
+export const displayMessageAfterWrongRound = (userAnswer, expectedAnswer, userName) => {
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'. Let's try again, ${userName}!`);
+};
+export const displayMessageAfterGameEnd = (userName) => {
+  console.log(`Congratulation, ${userName}!`);
+};
+
+export const getUserAnswer = () => {
+  const userAnswer = readlineSync.question('Your answer: ');
+  return userAnswer;
+};
